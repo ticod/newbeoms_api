8498194e3ad9972023e5707d43fc057fa4ad2bec
tico <lours2021@gmail.com>
2024-06-17 16:12:32 +0900

    Initial commit

---
+++ src/main/java/com/ticod/newbeoms_api/NewbeomsApiApplication.java
@@ -0,0 +1,13 @@
+package com.ticod.newbeoms_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewbeomsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewbeomsApiApplication.class, args);
+    }
+
+}
