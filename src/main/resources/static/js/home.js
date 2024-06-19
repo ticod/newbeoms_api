26c87160b5e5019ad41e05baa69063c713d24ac9
tico <lours2021@gmail.com>
2024-06-20 05:44:33 +0900

    Add home 페이지 추가
    관련 css, js 추가

---
+++ src/main/resources/static/js/home.js
@@ -0,0 +1,35 @@
+const newsMainList = document.getElementsByClassName("news-main");
+const newsLeftArrowButton = document.getElementById("news_left_arrow_button");
+const newsRightArrowButton = document.getElementById("news_right_arrow_button");
+const newsButton = document.getElementById("news_button");
+
+let newsCount = newsMainList.length - 1;
+
+function showNews(targetNewsCount) {
+    for (let i = 0; i < newsMainList.length; i++) {
+        if (i !== targetNewsCount) {
+            newsMainList[i].style.display = "none";
+        } else {
+            newsMainList[i].style.display = "block";
+        }
+    }
+}
+
+showNews(newsCount);
+
+newsLeftArrowButton.addEventListener('click', () => {
+    newsCount--;
+    if (newsCount < 0) {
+        newsCount = newsMainList.length - 1;
+    }
+    showNews(newsCount);
+});
+
+newsRightArrowButton.addEventListener('click', () => {
+    newsCount = (++newsCount) % newsMainList.length;
+    showNews(newsCount);
+});
+
+newsButton.addEventListener('click', () => {
+    window.location.href="layout.html";
+});
