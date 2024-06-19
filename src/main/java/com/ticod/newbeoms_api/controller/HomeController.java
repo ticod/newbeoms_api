a8250d29fe8cb0a2567900d281a8745e8795b16e
tico <lours2021@gmail.com>
2024-06-20 05:55:28 +0900

    Update HomeController
    타임리프 추가 및 home.html 추가에 따른
    컨트롤러 수정

--- src/main/java/com/ticod/newbeoms_api/controller/HomeController.java
+++ src/main/java/com/ticod/newbeoms_api/controller/HomeController.java
@@ -1,35 +1,14 @@
 package com.ticod.newbeoms_api.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class HomeController {
 
-    /**
-     * 해당 출간일의 기사 반환
-     */
-    @GetMapping("/publication/{date}")
-    public String getPublicationByDate(@RequestParam("date") String date) {
-        return date;
-    }
-
-    /**
-     * 출간일 및 기사 추가
-     */
-    @PostMapping("/publication")
-    public String addPublication() {
-        return "ok";
-    }
-
-    /**
-     * 해당 태그에 관련된 기사 반환
-     */
-    @GetMapping("/news/{tags}")
-    public String getNewsByTag(@RequestParam("tags") String tag) {
-        return tag;
+    @GetMapping("/")
+    public String home() {
+        return "home";
     }
 
 }
