520e1ec22364bea0a7ec830a0747ebe23d5127d8
tico <lours2021@gmail.com>
2024-06-20 07:20:38 +0900

    Refactor repository
    필요없는 import 제거

--- src/main/java/com/ticod/newbeoms_api/repository/PublicationRepository.java
+++ src/main/java/com/ticod/newbeoms_api/repository/PublicationRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
-import java.util.Optional;
 
 @Repository
 public interface PublicationRepository extends JpaRepository<Publication, Long> {
