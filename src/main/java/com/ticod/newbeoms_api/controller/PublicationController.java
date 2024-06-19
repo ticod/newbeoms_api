043a102f79741404f3d0e326608e1314a3d70225
tico <lours2021@gmail.com>
2024-06-20 07:25:30 +0900

    Update PublicationController
    RestController -> Controller 변경 및 겹치는 URI 변경

--- src/main/java/com/ticod/newbeoms_api/controller/PublicationController.java
+++ src/main/java/com/ticod/newbeoms_api/controller/PublicationController.java
@@ -1,17 +1,23 @@
 package com.ticod.newbeoms_api.controller;
 
 import com.ticod.newbeoms_api.dto.PublicationDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+import java.time.LocalDate;
+
+@Slf4j
+@Controller
 public class PublicationController {
 
     /**
      * 해당 출간일의 기사 반환
      */
     @GetMapping("/publication/{date}")
-    public String getPublicationByDate(@RequestParam("date") String date) {
-        return date;
+    public String getPublicationByDate(@PathVariable("date") LocalDate date) {
+        log.info(String.valueOf(date));
+        return "news";
     }
 
     /**
@@ -25,8 +31,8 @@ public class PublicationController {
     /**
      * 해당 태그에 관련된 기사 반환
      */
-    @GetMapping("/publication/{tags}")
-    public String getNewsByTag(@RequestParam("tags") String tag) {
+    @GetMapping("/publication/news/{tags}")
+    public String getNewsByTag(@PathVariable("tags") String tag) {
         return tag;
     }
 
