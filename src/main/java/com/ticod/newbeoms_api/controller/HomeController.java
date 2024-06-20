8edaf129b60808bb08ae2dafdabcfd0fb616540c
tico <lours2021@gmail.com>
2024-06-20 16:43:06 +0900

    Update HomeController
    home에 publicationDateDto list를 model에 추가

--- src/main/java/com/ticod/newbeoms_api/controller/HomeController.java
+++ src/main/java/com/ticod/newbeoms_api/controller/HomeController.java
@@ -1,13 +1,27 @@
 package com.ticod.newbeoms_api.controller;
 
+import com.ticod.newbeoms_api.dto.PublicationDateDto;
+import com.ticod.newbeoms_api.service.PublicationService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
 
+    private final PublicationService publicationService;
+
+    public HomeController(PublicationService publicationService) {
+        this.publicationService = publicationService;
+    }
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        List<PublicationDateDto> publicationDates = publicationService.getPublicationDates();
+        model.addAttribute("publicationDates", publicationDates);
         return "home";
     }
 
