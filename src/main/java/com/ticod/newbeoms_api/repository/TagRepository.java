3167d889247f59ca22c0afc5ed8a9fd49b9aa1cf
tico <lours2021@gmail.com>
2024-06-21 15:59:37 +0900

    Update TagRepository
    findByTags 추가
    - 문자열을 바탕으로 Tag의 content 필드와 비교해서
      검색된 List<Tag> 반환
    findByContent 추가
    - 하나의 content로 하나의 Tag select, 반환

--- src/main/java/com/ticod/newbeoms_api/repository/TagRepository.java
+++ src/main/java/com/ticod/newbeoms_api/repository/TagRepository.java
@@ -2,6 +2,14 @@ package com.ticod.newbeoms_api.repository;
 
 import com.ticod.newbeoms_api.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
+
+    @Query("select distinct t from Tag t where t.content in (:tags)")
+    List<Tag> findByTags(List<String> tags);
+    Tag findByContent(String content);
+
 }
