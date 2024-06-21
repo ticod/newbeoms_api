e0c60372910eaad5d84a58ab5dc0a4470f31a7bd
tico <lours2021@gmail.com>
2024-06-21 18:44:24 +0900

    Update home.html, home.js
    home.html의 검색 창 기능 추가

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -3,7 +3,12 @@ const newsLeftArrowButton = document.getElementById("news_left_arrow_button");
 const newsRightArrowButton = document.getElementById("news_right_arrow_button");
 const newsButton = document.getElementById("news_button");
 
+const searchInput = document.getElementById("search_input")
+const searchButton = document.getElementById("search_button")
+
 const PUBLICATION_URI = "/publication/";
+const SEARCH_URI = "/publication/news/";
+
 
 let newsCount = newsMainList.length - 1;
 
@@ -35,3 +40,15 @@ newsRightArrowButton.addEventListener('click', () => {
 newsButton.addEventListener('click', () => {
     window.location.href = PUBLICATION_URI + newsMainList[newsCount].id;
 });
+
+// Search button
+function searchEvent() {
+    window.location.href = SEARCH_URI + searchInput.value;
+}
+
+searchButton.addEventListener('click', searchEvent);
+searchInput.addEventListener('keydown', (e) => {
+    if (e.key === "Enter") {
+        searchEvent();
+    }
+})
