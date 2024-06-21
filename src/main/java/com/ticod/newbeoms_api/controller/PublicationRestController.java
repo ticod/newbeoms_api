0f3002f6f2d02fb94a7003c681030bb9b50a07a4
tico <lours2021@gmail.com>
2024-06-21 16:04:33 +0900

    Add PublicationRestController
    getNewsByTag 추가

--- src/main/java/com/ticod/newbeoms_api/controller/PublicationRestController.java
+++ src/main/java/com/ticod/newbeoms_api/controller/PublicationRestController.java
@@ -1,12 +1,18 @@
 package com.ticod.newbeoms_api.controller;
 
+import com.ticod.newbeoms_api.dto.NewsDto;
 import com.ticod.newbeoms_api.dto.PublicationDto;
+import com.ticod.newbeoms_api.entity.Tag;
 import com.ticod.newbeoms_api.service.PublicationService;
-import org.springframework.ui.Model;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
+@Slf4j
 @RestController
 public class PublicationRestController {
 
@@ -21,6 +27,7 @@ public class PublicationRestController {
      */
     @PostMapping("/rest/publication")
     public String addPublication(@RequestBody PublicationDto publicationDto) {
+        log.info(publicationDto.toString());
         publicationService.addPublcation(publicationDto);
         return "ok";
     }
@@ -31,6 +38,25 @@ public class PublicationRestController {
     @GetMapping("/rest/publication/{date}")
     public PublicationDto getPublicationByDate(@PathVariable("date") LocalDate date) {
         return publicationService.getPublicationDto(date);
+    }
+
+    /**
+     * 해당 태그에 관련된 기사 반환
+     */
+    @GetMapping("/rest/publication/news/{tags}")
+    public List<NewsDto> getNewsByTag(@PathVariable("tags") String uriTag) {
+        List<String> tags = List.of(uriTag.trim().split(" "));
+        log.info(tags.toString());
+        List<Tag> tagList = publicationService.getTagsByContents(tags);
+        log.info(tagList.toString());
+        Set<NewsDto> result = new HashSet<>();
+        for (Tag tag : tagList) {
+            result.addAll(tag.getNewsTagList().stream().map(
+                    newsTag -> NewsDto.from(newsTag.getNews())
+            ).toList());
+        }
+        log.info(result.toString());
+        return result.stream().toList();
     }
 
 }
