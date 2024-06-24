cebbb5005f8234f67ea2bd15f4896574e624f277
tico <lours2021@gmail.com>
2024-06-25 01:52:48 +0900

    Update GossipDto toEntity
    Entity로 변환하는 메서드 추가

--- src/main/java/com/ticod/newbeoms_api/dto/GossipDto.java
+++ src/main/java/com/ticod/newbeoms_api/dto/GossipDto.java
@@ -2,11 +2,14 @@ package com.ticod.newbeoms_api.dto;
 
 import com.ticod.newbeoms_api.entity.Gossip;
 import com.ticod.newbeoms_api.entity.GossipLink;
+import com.ticod.newbeoms_api.entity.Publication;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Getter
 @Setter
@@ -33,6 +36,31 @@ public class GossipDto {
                 .title(gossip.getTitle())
                 .links(gossip.getGossipLinks().stream().map(GossipLink::getLink).toList())
                 .build();
+    }
+
+    public static Gossip toGossip(GossipDto gossipDto, Publication publication) {
+        return Gossip.builder()
+                .publication(publication)
+                .title(gossipDto.getTitle())
+                .build();
+    }
+
+    public static List<GossipLink> toGossipLinks(GossipDto gossipDto, Gossip gossip) {
+        return gossipDto.getLinks().stream()
+                .map(link -> GossipLink.builder()
+                        .gossip(gossip)
+                        .link(link)
+                        .build())
+                .toList();
+    }
+
+    public static Map<Gossip, List<GossipLink>> toEntityMap(GossipDto gossipDto, Publication publication) {
+        Gossip gossip = toGossip(gossipDto, publication);
+        List<GossipLink> gossipLinks = toGossipLinks(gossipDto, gossip);
+
+        Map<Gossip, List<GossipLink>> result = new HashMap<>();
+        result.put(gossip, gossipLinks);
+        return result;
     }
 
 }
