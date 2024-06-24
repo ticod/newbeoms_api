b76a39c1370c0402bfb52a5787b2de7bc78f9102
tico <lours2021@gmail.com>
2024-06-25 02:10:34 +0900

    Update ComingSoonDto toEntityMap
    ComingSoonDto ->
    Map<ComingSoon, List<ComingSoonContent>>
    변환 메서드 추가

--- src/main/java/com/ticod/newbeoms_api/dto/ComingSoonDto.java
+++ src/main/java/com/ticod/newbeoms_api/dto/ComingSoonDto.java
@@ -1,13 +1,14 @@
 package com.ticod.newbeoms_api.dto;
 
-import com.ticod.newbeoms_api.entity.ComingSoon;
-import com.ticod.newbeoms_api.entity.ComingSoonContent;
+import com.ticod.newbeoms_api.entity.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Getter
 @Setter
@@ -28,5 +29,30 @@ public class ComingSoonDto {
                 .contents(comingSoon.getComingSoonContents()
                         .stream().map(ComingSoonContent::getContent).toList())
                 .build();
+    }
+
+    public static ComingSoon toComingSoon(ComingSoonDto comingSoonDto, Publication publication) {
+        return ComingSoon.builder()
+                .publication(publication)
+                .comingSoonDate(comingSoonDto.getDate())
+                .build();
+    }
+
+    public static List<ComingSoonContent> toComingSoonContents(ComingSoonDto comingSoonDto, ComingSoon comingSoon) {
+        return comingSoonDto.getContents().stream()
+                .map(content -> ComingSoonContent.builder()
+                        .comingSoon(comingSoon)
+                        .content(content)
+                        .build())
+                .toList();
+    }
+
+    public static Map<ComingSoon, List<ComingSoonContent>> toEntityMap(ComingSoonDto comingSoonDto, Publication publication) {
+        ComingSoon comingSoon = toComingSoon(comingSoonDto, publication);
+        List<ComingSoonContent> comingSoonContents = toComingSoonContents(comingSoonDto, comingSoon);
+
+        Map<ComingSoon, List<ComingSoonContent>> result = new HashMap<>();
+        result.put(comingSoon, comingSoonContents);
+        return result;
     }
 }
